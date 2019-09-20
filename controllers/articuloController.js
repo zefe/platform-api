260d48d091b9e765f160dde7151a81a648d9b28c
Zeferino Torres <zeferino@relativity6.com>
2019-09-20 16:08:39 -0500

    add filter category and article

--- controllers/articuloController.js
+++ controllers/articuloController.js
@@ -2,7 +2,7 @@ import models from '../models';
 export default {
     add: async (req,res,next) =>{
         try {
-            const reg = await models.Categoria.create(req.body);
+            const reg = await models.Articulo.create(req.body);
             res.status(200).json(reg);
         } catch (e){
             res.status(500).send({
@@ -13,7 +13,8 @@ export default {
     },
     query: async (req,res,next) => {
         try {
-            const reg=await models.Categoria.findOne({_id:req.query._id});
+            const reg=await models.Articulo.findOne({_id:req.query._id})
+            .populate('categoria',{nombre:1});
             if (!reg){
                 res.status(404).send({
                     message: 'El registro no existe'
@@ -31,8 +32,9 @@ export default {
     list: async (req,res,next) => {
         try {
             let valor=req.query.valor;
-            const reg= await models.Categoria.find({$or: [{'nombre':new RegExp(valor, 'i')},{'descripcion': new RegExp(valor, 'i')}]},{createAt:0})
-            .sort({'createdAt': -1});
+            const reg=await models.Articulo.find({$or:[{'nombre':new RegExp(valor,'i')},{'descripcion':new RegExp(valor,'i')}]},{createdAt:0})
+            .populate('categoria',{nombre:1})
+            .sort({'createdAt':-1});
             res.status(200).json(reg);
         } catch(e){
             res.status(500).send({
@@ -43,7 +45,7 @@ export default {
     },
     update: async (req,res,next) => {
         try {
-            const reg = await models.Categoria.findByIdAndUpdate({_id:req.body._id},{nombre:req.body.nombre,descripcion:req.body.descripcion});
+            const reg = await models.Articulo.findByIdAndUpdate({_id:req.body._id},{categoria:req.body.categoria,codigo:req.body.codigo,nombre:req.body.nombre,descripcion:req.body.descripcion,precio_venta:req.body.precio_venta,stock:req.body.stock});
             res.status(200).json(reg);
         } catch(e){
             res.status(500).send({
@@ -54,7 +56,7 @@ export default {
     },
     remove: async (req,res,next) => {
         try {
-            const reg = await models.Categoria.findByIdAndDelete({_id:req.body._id});
+            const reg = await models.Articulo.findByIdAndDelete({_id:req.body._id});
             res.status(200).json(reg);
         } catch(e){
             res.status(500).send({
@@ -65,7 +67,7 @@ export default {
     },
     activate: async (req,res,next) => {
         try {
-            const reg = await models.Categoria.findByIdAndUpdate({_id:req.body._id},{estado:1});
+            const reg = await models.Articulo.findByIdAndUpdate({_id:req.body._id},{estado:1});
             res.status(200).json(reg);
         } catch(e){
             res.status(500).send({
@@ -76,7 +78,7 @@ export default {
     },
     deactivate:async (req,res,next) => {
         try {
-            const reg = await models.Categoria.findByIdAndUpdate({_id:req.body._id},{estado:0});
+            const reg = await models.Articulo.findByIdAndUpdate({_id:req.body._id},{estado:0});
             res.status(200).json(reg);
         } catch(e){
             res.status(500).send({
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import routerx from 'express-promise-router';
 import categoriaRouter from './categoria';
+import articuloRouter from './articulo';
 
 const router=routerx();
 
 router.use('/categoria',categoriaRouter);
+router.use('/articulo', articuloRouter);
 export default router;
